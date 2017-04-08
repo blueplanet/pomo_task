6e19df76ba01983caceccd97ac0d10707b1e2cd5
blueplanet <erguolinge@gmail.com>
2017-04-08 13:37:35 +0900

    rename

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails5base_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "pomo_task_#{Rails.env}"
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
